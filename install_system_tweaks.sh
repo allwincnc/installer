d0e5372498c8c00d18bc54a83734d3f34bc242d9
MX_Master <mikhail@vydrenko.ru>
2020-03-09 16:37:03 +0600

    + system tweaks installer

---
+++ install_system_tweaks.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# var list
+      NAME="System tweaks"
+    F_FILE="/etc/default/cpufrequtils"
+     F_APP="cpufreq-set"
+ F_APP_CHK="cpufrequtils"
+ F_APP_CMD="${F_APP} -g performance -r"
+
+
+
+
+# greetings
+echo "--- Installing '${NAME}' -------"
+
+
+
+
+# set max CPU frequency
+if [[ ! $($F_APP --help | grep $F_APP_CHK) ]]; then
+    echo "ERROR: Can't find the '${F_APP}' program (${0}:${LINENO})."
+    exit 1
+fi
+
+sudo $F_APP_CMD
+
+if [[ ! -f $F_FILE ]]; then
+    echo "ERROR: Can't find the '${F_FILE}' file (${0}:${LINENO})."
+    exit 1
+fi
+
+sudo sed -i -e "s/.*GOVERNOR=.*/GOVERNOR=performance/" $F_FILE
+
+
+
+
+echo "--- The '${NAME}' successfuly installed -------"
