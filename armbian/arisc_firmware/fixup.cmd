9816c964969cfd2ece4f5829aeaccd15a03cffdb
MX_Master <mikhail@vydrenko.ru>
2020-04-15 18:56:53 +0600

    * firmware loader update

--- armbian/arisc_firmware/fixup.cmd
+++ armbian/arisc_firmware/fixup.cmd
@@ -4,9 +4,11 @@
 
 setenv ARISC_blob_name "arisc-fw.code"
 
-# Allwinner H3 SoC
+# this data valid for Allwinner H2+/H3/H5 SoCs
+setenv tmp_addr 0x44001000
 setenv SRAM_A2_addr 0x00040000
 setenv R_CPUCFG_addr 0x01F01C00
+setenv fw_max_size 0xC000
 
 if test -e ${devtype} ${devnum} ${prefix}${ARISC_blob_name}; then
 	echo "Found the ARISC firmware blob `${prefix}${ARISC_blob_name}`"
@@ -14,7 +16,8 @@ if test -e ${devtype} ${devnum} ${prefix}${ARISC_blob_name}; then
 	echo "  asserting the ARISC core reset.."
 	mw.l ${R_CPUCFG_addr} 0x0 0x1
 	echo "  asserting reset done, trying to upload the blob to the SRAM A2 (${SRAM_A2_addr}).."
-	load ${devtype} ${devnum} ${SRAM_A2_addr} ${prefix}${ARISC_blob_name}
+	load ${devtype} ${devnum} ${tmp_addr} ${prefix}${ARISC_blob_name}
+	cp.b ${tmp_addr} ${SRAM_A2_addr} ${fw_max_size}
 	echo "  uploading done, de-asserting the ARISC core reset.."
 	mw.l ${R_CPUCFG_addr} 0x1 0x1
 	sleep 0.1
