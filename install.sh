820942e0616b22dc859dffddd273621b91d969b0
MX_Master <mikhail@vydrenko.ru>
2021-05-07 16:58:40 +0600

    + SoC name prompt

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ source tools.sh
 # var list
    NAME="allwincnc"
     ERR="0"
+   CHIP=""
   TASKS=( \
-        "cmd:./install_sys_tweaks.sh,   do:1,   arg:" \
-        "cmd:./install_sys_lang.sh,     do:0,   arg:" \
-        "cmd:./install_kernel.sh,       do:1,   arg:" \
-        "cmd:./install_linuxcnc.sh,     do:1,   arg:en" \
-        "cmd:./install_drv.sh,          do:1,   arg:" \
-        "cmd:./install_cfg.sh,          do:1,   arg:" \
-        "cmd:./install_fw.sh,           do:1,   arg:" \
+        "cmd:./install_sys_tweaks.sh,   do:1,  arg:" \
+        "cmd:./install_sys_lang.sh,     do:1,  arg:" \
+        "cmd:./install_kernel.sh,       do:1,  arg:" \
+        "cmd:./install_linuxcnc.sh,     do:1,  arg:" \
+        "cmd:./install_drv.sh,          do:1,  arg:" \
+        "cmd:./install_cfg.sh,          do:1,  arg:" \
+        "cmd:./install_fw.sh,           do:1,  arg:" \
         )
 
 
@@ -24,6 +25,49 @@ log "--------------------------------------------------------------"
 log "--- Installing **${NAME}** -------"
 log "--------------------------------------------------------"
 log ""
+
+
+
+
+# select a SoC from the arguments list
+if [[ $# != 0 ]]; then
+    for arg in $*; do
+        case $arg in
+            "h3") CHIP="h3"; ;;
+            "H3") CHIP="h3"; ;;
+            "h5") CHIP="h5"; ;;
+            "H5") CHIP="h5"; ;;
+            "h6") CHIP="h6"; ;;
+            "H6") CHIP="h6"; ;;
+        esac
+    done
+fi
+
+# if no SoC selected yet
+while [[ "${CHIP}" != "1"  && "${CHIP}" != "2"  && "${CHIP}" != "3"  && \
+         "${CHIP}" != "h3" && "${CHIP}" != "H3" && \
+         "${CHIP}" != "h5" && "${CHIP}" != "H5" && \
+         "${CHIP}" != "h6" && "${CHIP}" != "H6" ]]; do
+    log     "Please select your SoC:"
+    log     "  1: Allwinner H3"
+    log     "  2: Allwinner H5"
+    log     "  3: Allwinner H6"
+    read -p "SoC: " CHIP
+done
+
+# set SoC
+case "${CHIP}" in
+    "1")  CHIP="h3"; ;;
+    "2")  CHIP="h5"; ;;
+    "3")  CHIP="h6"; ;;
+    "h3") CHIP="h3"; ;;
+    "H3") CHIP="h3"; ;;
+    "h5") CHIP="h5"; ;;
+    "H5") CHIP="h5"; ;;
+    "h6") CHIP="h6"; ;;
+    "H6") CHIP="h6"; ;;
+    *)    CHIP="h3"; ;;
+esac
 
 
 
@@ -41,7 +85,7 @@ for task in "${TASKS[@]}"; do
     if [[ $do == "1" || $do == "y" || $do == "Y" ]]; then
         cmd=$(echo "$task" | sed -e "s/.*cmd:\([^,]*\).*/\1/")
         arg=$(echo "$task" | sed -e "s/.*arg:\([^,]*\).*/\1/")
-        "$cmd" "$arg"
+        "$cmd" "${CHIP}" "$arg"
         if [[ $? != "0" ]]; then ERR="1"; fi
     fi
 done
--- install_drv.sh
+++ install_drv.sh
@@ -4,6 +4,7 @@ source tools.sh
 
 # var list
       NAME="ARISC driver for the LinuxCNC"
+      CHIP=""
    CUR_DIR=$(pwd)
    SRC_DIR="linuxcnc/drv"
  TARGET_ID="0"
@@ -16,6 +17,52 @@ source tools.sh
 # greetings
 log ""
 log "--- Installing **${NAME}** -------"
+
+
+
+
+# select a SoC from the arguments list
+if [[ $# != 0 ]]; then
+    for arg in $*; do
+        case $arg in
+            "h3") CHIP="h3"; ;;
+            "H3") CHIP="h3"; ;;
+            "h5") CHIP="h5"; ;;
+            "H5") CHIP="h5"; ;;
+            "h6") CHIP="h6"; ;;
+            "H6") CHIP="h6"; ;;
+        esac
+    done
+fi
+
+# if no SoC selected yet
+while [[ "${CHIP}" != "1"  && "${CHIP}" != "2"  && "${CHIP}" != "3"  && \
+         "${CHIP}" != "h3" && "${CHIP}" != "H3" && \
+         "${CHIP}" != "h5" && "${CHIP}" != "H5" && \
+         "${CHIP}" != "h6" && "${CHIP}" != "H6" ]]; do
+    log     "Please select your SoC:"
+    log     "  1: Allwinner H3"
+    log     "  2: Allwinner H5"
+    log     "  3: Allwinner H6"
+    read -p "SoC: " CHIP
+done
+
+# set SoC
+case "${CHIP}" in
+    "1")  CHIP="h3"; ;;
+    "2")  CHIP="h5"; ;;
+    "3")  CHIP="h6"; ;;
+    "h3") CHIP="h3"; ;;
+    "H3") CHIP="h3"; ;;
+    "h5") CHIP="h5"; ;;
+    "H5") CHIP="h5"; ;;
+    "h6") CHIP="h6"; ;;
+    "H6") CHIP="h6"; ;;
+    *)    CHIP="h3"; ;;
+esac
+
+# setup sources folder based on the SoC name
+SRC_DIR="${SRC_DIR}/${CHIP}"
 
 
 
