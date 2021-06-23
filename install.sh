6f0bedd1c82dab3994ad6a28d0dd583552c80edc
MX_Master <mikhail@vydrenko.ru>
2021-06-23 17:44:10 +0600

    * enable LKM installer

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ source tools.sh
         "cmd:./install_sys_tweaks.sh,   do:1,  arg:" \
         "cmd:./install_sys_lang.sh,     do:1,  arg:" \
         "cmd:./install_kernel.sh,       do:1,  arg:" \
-        "cmd:./install_lkm.sh,          do:0,  arg:" \
+        "cmd:./install_lkm.sh,          do:1,  arg:" \
         "cmd:./install_linuxcnc.sh,     do:1,  arg:" \
         "cmd:./install_drv.sh,          do:1,  arg:" \
         "cmd:./install_cfg.sh,          do:1,  arg:" \
