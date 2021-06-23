a98bc8f28c8fda21094e4ed2049a6a5109729351
MX_Master <mikhail@vydrenko.ru>
2021-06-23 17:59:32 +0600

    * script incorrect content fix

--- install_lkm.sh
+++ install_lkm.sh
@@ -3,25 +3,41 @@
 source tools.sh
 
 # var list
-      NAME="ARISC firmware"
+      NAME="ARISC kernel module"
    CUR_DIR=$(pwd)
- LKMD_NAME="/dev/arisc"
- ALL_FILES=("/boot/arisc-fw.code")
+   DST_DIR="/boot/allwincnc"
+ DST_DIR_E="\\/boot\\/allwincnc"
+   SRC_DIR="./armbian/lkm"
+ ALL_FILES=("Makefile" "arisc_admin.c" "arisc_lkm_installer.sh" "arisc_fw_loader.sh")
 
 
 
 
 # greetings
 log ""
-log "--- Starting **${NAME}** -------"
+log "--- Installing **${NAME}** -------"
 
 
 
 
-# check files
+# check/create folders
+if [[ ! -d "${DST_DIR}" ]]; then
+    sudo mkdir "${DST_DIR}"
+fi
+
+
+
+
+# check/copy files
 for file in ${ALL_FILES[*]}; do
-    if [[ ! -f "${file}" ]]; then
-        log "!!ERROR!!: Can't find the **${file}** file [**${0}:${LINENO}**]."
+    if [[ ! -f "${SRC_DIR}/${file}" ]]; then
+        log "!!ERROR!!: Can't find the **${SRC_DIR}/${file}** file [**${0}:${LINENO}**]."
+        exit 1
+    fi
+
+    sudo cp -f "${SRC_DIR}/${file}" "${DST_DIR}/${file}"
+    if [[ ! -f "${DST_DIR}/${file}" ]]; then
+        log "!!ERROR!!: Can't create the **${DST_DIR}/${file}** file [**${0}:${LINENO}**]."
         exit 1
     fi
 done
@@ -29,22 +45,16 @@ done
 
 
 
-# run the ARISC core with desired firmware
-if [[ ! -f "${LKMD_NAME}" ]]; then
-    log "!!ERROR!!: Can't find the **${LKMD_NAME}** system device [**${0}:${LINENO}**]."
+# start LKM installer on reboot
+if [[ ! -f "/etc/rc.local" ]]; then
+    log "!!ERROR!!: Can't find the **/etc/rc.local** file [**${0}:${LINENO}**]."
     exit 1
 fi
 
-echo "stop erase /boot/arisc-fw.code upload start status" > "${LKMD_NAME}"
-sleep 1
-
-if [[ $(cat ${LKMD_NAME} | grep error) ]]; then
-    log "!!ERROR!!: ARISC core's firmware upload and run failed [**${0}:${LINENO}**]."
-    exit 1
-fi
+sudo sed -i -e "s/^exit 0/${DST_DIR}\/arisc_lkm_installer\.sh\nexit 0/" "/etc/rc.local"
 
 
 
 
-log "--- **${NAME}** ++successfully running++ -------"
+log "--- **${NAME}** ++successfully installed++ -------"
 log ""
